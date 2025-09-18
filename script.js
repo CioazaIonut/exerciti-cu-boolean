97be88969e93f5ecefd16ff5e6e75799e9a48855
CioazaIoan <cioazai@yahoo.com>
2025-09-18 19:51:38 +0300

    primul

---
+++ script.js
@@ -0,0 +1,40 @@
+// Crearea variabilelor boolean și verificarea valorii:
+
+let esteDisponibil = true;
+let esteVindea = false;
+
+console.log(esteDisponibil);
+console.log(esteVindea);
+
+// Verificare condiție simplă:
+
+let arePermis = true;
+
+console.log(arePermis === true);
+
+// Comparare cu operatori:
+
+let var1 = 10;
+let var2 = 20;
+
+console.log(var1 < var2);
+
+// Combinație cu operatorul ! (negare):
+
+let esteOnline = false;
+
+console.log(!esteOnline);
+
+// Verificare dacă două condiții sunt adevărate:
+
+let varstaUnu = 25;
+let detinePermis = true;
+
+console.log(varstaUnu >= 18 && detinePermis);
+
+// Exercițiu: Scrie o condiție care verifică dacă o persoană e adult și are permis:
+
+let varsta = 30;
+let permis = false;
+
+console.log(varsta >= 18 && permis);
